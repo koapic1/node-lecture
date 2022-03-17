4dfe672fa1d6cc18af43f2a404a690981259bcb9
koapic1 <koapic1@gmail.com>
2022-03-17 10:44:49 +0900

    node day2

--- express/index.js
+++ express/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
 app.set("port", process.env.PORT || 3003);
+
+app.use(express.static(__dirname + "/public")); // 정적 파일들 관리
+
 app.get("/", (req, res) => {
     //res.send("<h1>Hello Express</h1>");
     res.sendFile(__dirname + "/index.html");
-    console.log("nodemon으로 서버 실행중");
+    //console.log("nodemon으로 서버 실행중");
+    /*
+    const output = `
+    <h1>INDEX</h1>
+    <p>Index.html 입니다.</p>
+    <ul>
+        <li><a href="/">Index</a></li>
+        <li><a href="/board">Board</a></li>
+        <li><a href="/user">User</a></li>
+    </ul>
+    <img src = ./node.png />
+    `;
+    res.send(output);
+    */
 });
 app.get("/board", (req, res) => {
     //res.send("<h1>Hello Express</h1>");
